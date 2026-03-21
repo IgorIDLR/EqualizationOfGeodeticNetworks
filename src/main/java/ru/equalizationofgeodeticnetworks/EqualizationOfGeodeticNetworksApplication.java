bcc07c3620a7439d254d5b11aee1abbe5ffd9a17
Igor <Cyrax112@yandex.ru>
2026-03-21 16:59:49 +0300

    comit create project

---
+++ src/main/java/ru/equalizationofgeodeticnetworks/EqualizationOfGeodeticNetworksApplication.java
@@ -0,0 +1,13 @@
+package ru.equalizationofgeodeticnetworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EqualizationOfGeodeticNetworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EqualizationOfGeodeticNetworksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/equalizationofgeodeticnetworks/EqualizationOfGeodeticNetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.equalizationofgeodeticnetworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EqualizationOfGeodeticNetworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
